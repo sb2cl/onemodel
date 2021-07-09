22fe102fa49071a03c962106020537a0d71bdb62
FernandoNobel <fersann1@upv.es>
2021-07-09 12:55:55 +0200

    Lexer base implementer

--- Lexer.py
+++ Lexer.py
@@ -1,7 +1,8 @@
 from enum import Enum, auto
+from Position import Position
 
 class TokenType(Enum):
-    """ Token types defined.
+    """ TOKENTYPE
 
     This class defines all the token types available for the lexer.
     """
@@ -31,6 +32,111 @@ class TokenType(Enum):
     NEW_LINE            = auto()
     END_OF_FILE         = auto()
 
+# Keywords reserved for tokens.
+KEYWORDS = [
+        'VAR',
+        'AND',
+        'OR',
+        'NOT',
+        'IF',
+        'ELIF',
+        'ELSE',
+        'FOR',
+        'TO',
+        'STEP',
+        'WHILE',
+        'FUN',
+        'THEN',
+        'END',
+        'RETURN',
+        'CONTINUE',
+        'BREAK',
+        ]
 
-     
+class Token:
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+        self.type = type_
+        self.value = value
 
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
+
+        if pos_end:
+            self.pos_end = pos_end.copy()
+
+    def matches(self, type_, value):
+        return self.type == type_ and self.value == value
+  
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+class Lexer:
+    """ LEXER
+
+    This class takes input text and generates a list of tokens.
+    """
+
+    def __init__(self,fn,text):
+        """
+        @brief: Constructor of Lexer.
+        
+        @param: fn   Filename of the text (used in error generation).
+              : text Input text to convert into tokens.
+        """
+        self.fn = fn
+        self.text = text
+        # Init the position
+        self.pos = Position(-1,0,-1,fn,text)
+        # Current char to be processed by the Lexer.
+        self.current_char = None
+        # Advance to the first char of the input text.
+        self.advance()
+
+    def advance(self):
+        """ ADVANCE
+        @brief: Update self.current_char with the next char to be processed.
+        
+        @return: None
+        """
+        # Keep track of the position in the file
+        self.pos.advance(self.current_char)
+        # Update current_char
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+    def make_tokens(self):
+        """ MAKE_TOKENS
+        @brief: Process all the input text and make the corresponding tokens.
+        
+        @return: tokens Array with the tokens.
+               : error  ExpectedCharError, IllegalCharError, or None
+        """
+        tokens = []
+
+        # Process all the chars one by one
+        while self.current_char != None:
+            # Remove white space
+            if self.current_char in ' \t':
+                self.advance()
+
+            elif self.current_char == '+':
+                tokens.append(Token(TokenType.PLUS, pos_start=self.pos))
+                self.advance()
+
+        # Add end of file token.
+        tokens.append(Token(TokenType.END_OF_FILE, pos_start=self.pos))
+        return tokens, None
+
+
+
+                
+        
+
+                
+        
+                
+        
+
+    
---
+++ Position.py
@@ -0,0 +1,49 @@
+class Position:
+    """ POSITION
+
+    This class keep track of the position in files for pin pointing errors.
+    """
+
+
+    def __init__(self,idx, ln, col, fn, ftxt):
+        """
+        @brief: Constructor of Position class.
+        
+        @param: idx  Current index in the text.
+              : ln   Current line in the text.
+              : col  Current column in the text.
+              : fn   Filename.
+              : ftxt Text converted into one string.
+        """
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self,current_char=None):
+        """ ADVANCE
+        @brief: Advance one char in the text position.
+        
+        @param: current_char Current char read.
+                
+        @return: Position 
+        """
+        self.idx += 1
+        self.col += 1
+
+        # If newline is read
+        if current_char == '\n':
+            # Increase line count and reset column.
+            self.ln += 1
+            self.col = 0
+    
+        return self
+
+    def copy(self):
+        """ COPY
+        @brief: Copy method
+        
+        @return: Copy position
+        """
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
