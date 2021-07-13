301b7755970e0ccb0b0d3591ad9c1570ddea8c2d
FernandoNobel <fersann1@upv.es>
2021-07-13 09:44:38 +0200

    Add test for number node

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,15 +1,32 @@
 import unittest
 from parser_ import Parser
 from tokens import Token, TokenType
+from nodes import *
 
 class TestParser(unittest.TestCase):
 
     def test_empty(self):
-        result = [
+        tokens = [
                 Token(TokenType.END_OF_FILE)
                 ]
 
-        parser = Parser(result)
+        parser = Parser(tokens)
         res = parser.parse()
 
         self.assertEqual(res.node, None)
+
+    def test_factor(self):
+        tokens = [
+                Token(TokenType.NUMBER,1),
+                Token(TokenType.END_OF_FILE)
+                ]
+
+        parser = Parser(tokens)
+        res = parser.parse()
+
+        self.assertEqual(res.node, 
+            NumberNode(Token(TokenType.NUMBER,1))
+            )
+
+
+
