d68c7faf08400c0a01bd23990303ed2ee4164569
FernandoNobel <fersann1@upv.es>
2021-07-14 12:50:27 +0200

    Add binaryOperationNode

--- nodes.py
+++ nodes.py
@@ -27,48 +27,73 @@ class NumberNode:
         """
         return f"{self.token.value}"
 
-@dataclass
-class AddNode:
-    node_a: any
-    node_b: any
+class BinaryOperationNode:
+    """ BINARYOPERATIONNODE
 
-    def __repr__(self):
-        return f"({self.node_a}+{self.node_b})"
+    Definition of a generic binary operation node.
+    """
+    def __init__(self,node_a,operation_token,node_b):
+        """ __INIT__
+        @brief: Constructor of BinaryOperationNode.
+        
+        @param: node_a          First operation node.
+              : operation_token Operation token.
+              : node_b          Second operation node.
+                
+        @return: BinaryOperationNode
+        """
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
 
-@dataclass
-class SubtractNode:
-    node_a: any
-    node_b: any
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
-    def __repr__(self):
-        return f"({self.node_a}-{self.node_b})"
+        def __repr__(self):
+            return f'({self.node_a}, {self.operation_token}, {self.node_b})'
 
-@dataclass
-class MultiplyNode:
-    node_a: any
-    node_b: any
-
-    def __repr__(self):
-        return f"({self.node_a}*{self.node_b})"
-
-@dataclass
-class DivideNode:
-    node_a: any
-    node_b: any
-
-    def __repr__(self):
-        return f"({self.node_a}/{self.node_b})"
-
-@dataclass
-class PlusNode:
-    node: any
-
-    def __repr__(self):
-        return f"(+{self.node})"
-    
-@dataclass
-class MinusNode:
-    node: any
-
-    def __repr__(self):
-        return f"(-{self.node})"
+#@dataclass
+#class AddNode:
+#    node_a: any
+#    node_b: any
+#
+#    def __repr__(self):
+#        return f"({self.node_a}+{self.node_b})"
+#
+#@dataclass
+#class SubtractNode:
+#    node_a: any
+#    node_b: any
+#
+#    def __repr__(self):
+#        return f"({self.node_a}-{self.node_b})"
+#
+#@dataclass
+#class MultiplyNode:
+#    node_a: any
+#    node_b: any
+#
+#    def __repr__(self):
+#        return f"({self.node_a}*{self.node_b})"
+#
+#@dataclass
+#class DivideNode:
+#    node_a: any
+#    node_b: any
+#
+#    def __repr__(self):
+#        return f"({self.node_a}/{self.node_b})"
+#
+#@dataclass
+#class PlusNode:
+#    node: any
+#
+#    def __repr__(self):
+#        return f"(+{self.node})"
+#    
+#@dataclass
+#class MinusNode:
+#    node: any
+#
+#    def __repr__(self):
+#        return f"(-{self.node})"
