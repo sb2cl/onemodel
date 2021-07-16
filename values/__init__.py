5f0f808d3829141c7e5a2726c8ea8a5b59dc6d81
FernandoNobel <fersann1@upv.es>
2021-07-16 16:36:27 +0200

    Moved Values to a folder

---
+++ values/__init__.py
@@ -0,0 +1,3 @@
+from values.value import Value
+from values.number import Number
+from values.function import Function
--- values/value.py
+++ values/value.py
@@ -1,8 +1,5 @@
 from errors import *
-
 from runTimeResult import RunTimeResult
-
-import math
 
 class Value:
     def __init__(self):
@@ -61,7 +58,7 @@ class Value:
         return None, self.illegal_operation()
 
     def execute(self, args):
-        return RTResult().failure(self.illegal_operation())
+        return RunTimeResult().failure(self.illegal_operation())
 
     def copy(self):
         raise Exception('No copy method defined')
@@ -76,136 +73,3 @@ class Value:
                 'Illegal operation',
                 self.context
                 )
-
-class Number(Value):
-
-    def __init__(self, value):
-        super().__init__()
-        self.value = float(value)
-
-    def added_to(self, other):
-        if isinstance(other, Number):
-            return Number(self.value + other.value).set_context(self.context), None
-
-    def subbed_by(self, other):
-        if isinstance(other, Number):
-            return Number(self.value - other.value).set_context(self.context), None
-
-    def multed_by(self, other):
-        if isinstance(other, Number):
-            return Number(self.value * other.value).set_context(self.context), None
-
-    def dived_by(self, other):
-        if isinstance(other, Number):
-            if other.value == 0:
-                return None, RunTimeError(
-                    other.pos_start, other.pos_end,
-                    'Division by zero',
-                    self.context
-                    )
-
-            return Number(self.value / other.value).set_context(self.context), None
-
-    def powed_by(self, other):
-        if isinstance(other, Number):
-            return Number(self.value ** other.value).set_context(self.context), None
-
-    def get_comparison_eq(self, other):
-        if isinstance(other, Number):
-            return Number(self.value == other.value).set_context(self.context), None
-
-    def get_comparison_ne(self, other):
-        if isinstance(other, Number):
-            return Number(self.value != other.value).set_context(self.context), None
-
-    def get_comparison_lt(self, other):
-        if isinstance(other, Number):
-            return Number(self.value < other.value).set_context(self.context), None
-
-    def get_comparison_gt(self, other):
-        if isinstance(other, Number):
-            return Number(self.value > other.value).set_context(self.context), None
-
-    def get_comparison_lte(self, other):
-        if isinstance(other, Number):
-            return Number(self.value <= other.value).set_context(self.context), None
-
-    def get_comparison_gte(self, other):
-        if isinstance(other, Number):
-            return Number(self.value >= other.value).set_context(self.context), None
-
-    def anded_by(self, other):
-        if isinstance(other, Number):
-            return Number(self.value and other.value).set_context(self.context), None
-
-    def ored_by(self, other):
-        if isinstance(other, Number):
-            return Number(self.value or other.value).set_context(self.context), None
-
-    def notted(self):
-        return Number(1 if self.value == 0 else 0).set_context(self.context), None
-
-    def copy(self):
-        copy = Number(self.value)
-        copy.set_pos(self.pos_start, self.pos_end)
-        copy.set_context(self.context)
-        return copy
-
-    def is_true(self):
-        return self.value != 0
-
-    def __repr__(self):
-        return f"{self.value}"
-
-Number.null    = Number(0)
-Number.false   = Number(0)
-Number.true    = Number(1)
-Number.math_PI = Number(math.pi)
-
-class Function(Value):
-    def __init__(self, name, body_node, arg_names):
-        super().__init__()
-        self.name = name or "<anonymous>"
-        self.body_node = body_node
-        self.arg_names = arg_names
-
-    def execute(self, args):
-        res = RunTimeResult()
-        interpreter = Interpreter()
-        new_context = Context(self.name, self.context, self.pos_start)
-        new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
-
-        if len(args) > len(self.arg_names):
-            return res.failure(RunTimeError(
-                self.pos_start, self.pos_end,
-                f"{len(args) - len(self.arg_names)} too many args passed into '{self.name}'",
-                self.context
-            ))
-        
-        if len(args) < len(self.arg_names):
-            return res.failure(RunTimeError(
-                self.pos_start, self.pos_end,
-                f"{len(self.arg_names) - len(args)} too few args passed into '{self.name}'",
-                self.context
-            ))
-
-        for i in range(len(args)):
-            arg_name = self.arg_names[i]
-            arg_value = args[i]
-            arg_value.set_context(new_context)
-            new_context.symbol_table.set(arg_name, arg_value)
-
-        value = res.register(interpreter.visit(self.body_node, new_context))
-        if res.error: return res
-        return res.success(value)
-
-    def copy(self):
-        copy = Function(self.name, self.body_node, self.arg_names)
-        copy.set_context(self.context)
-        copy.set_pos(self.pos_start, self.pos_end)
-        return copy
-
-    def __repr__(self):
-        return f"<function {self.name}>"
-
-
