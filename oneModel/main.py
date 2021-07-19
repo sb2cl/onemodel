87dd6a2990c3a4cd4702e66a2d09f7d97d81eba1
FernandoNobel <fersann1@upv.es>
2021-07-19 13:28:49 +0200

    updated shell

--- oneModel/main.py
+++ oneModel/main.py
@@ -9,9 +9,7 @@ while True:
     if error:
         print(error.as_string())
     elif result:
-        # TODO: recover the result.elements
-        print(repr(result))
-        #if len(result.elements) == 1:
-        #    print(repr(result.elements[0]))
-        #else:
-        #    print(repr(result))
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
