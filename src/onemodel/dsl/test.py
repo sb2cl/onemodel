8ec51f60a98389eda30c763bd3bcf13f7673b016
FernandoNobel <fersann1@upv.es>
2021-08-12 10:24:27 +0200

    Add equation and variables to Tatsu

--- src/onemodel/dsl/test.py
+++ src/onemodel/dsl/test.py
@@ -5,11 +5,14 @@ from tatsu.ast import AST
 
 from onemodel.onemodel import OneModel
 from onemodel.parameter import Parameter
+from onemodel.variable import Variable
+from onemodel.equation import Equation, EquationType
 from onemodel.export.matlab.matlab import Matlab
 
 class OnemodelSemantics(object):
     def __init__(self):
         self.onemodel = OneModel('test')
+        self.equation_num = 0
 
     def number(self, ast):
         return float(ast)
@@ -17,34 +20,77 @@ class OnemodelSemantics(object):
     def string(self, ast):
         return str(ast)
 
+    def operator(self, ast):
+        return str(ast)
+    
     def identifier(self, ast):
-        name = str(ast)
-        value = 'Identifier not defined'
-        return name, value
+        return str(ast)
+
+    def math_expr(self, ast):
+        expr = ''
+        for item in ast:
+            expr += str(item)
+
+        return expr
 
     def parameter(self, ast):
-        p = Parameter(ast.name[0])
+        p = Parameter(ast.name)
         p.value = str(ast.value)
         p.units = ast.units
         p.comment = ast.comment
         self.onemodel.add(p)
 
-        return p
+    def variable(self, ast):
+        v = Variable(ast.name)
+        v.value = str(ast.value)
+        v.units = ast.units
+        v.comment = ast.comment
+        self.onemodel.add(v)
+
+        return v
+
+    def equation_ode(self, ast):
+        e = Equation(f'eq_{self.equation_num}')
+        self.equation_num += 1
+        e.equation_type = EquationType.ODE
+        e.variable_name = ast.name
+        e.value = ast.eqn
+        e.comment = ast.comment
+        self.onemodel.add(e)
+
+    def equation_susbtitution(self, ast):
+        e = Equation(f'eq_{self.equation_num}')
+        self.equation_num += 1
+        e.equation_type = EquationType.SUBSTITUTION
+        e.variable_name = ast.name
+        e.value = ast.eqn
+        e.comment = ast.comment
+        self.onemodel.add(e)
+
+    def equation_algebraic(self, ast):
+        e = Equation(f'eq_{self.equation_num}')
+        self.equation_num += 1
+        e.equation_type = EquationType.ALGEBRAIC
+        e.variable_name = ast.name
+        e.value = ast.eqn
+        e.comment = ast.comment
+        self.onemodel.add(e)
+
+    def statement(self, ast):
+        return ast
 
 def main(data):
-    line = 'd1 = {1 , "1/t"} "Degradation time"'
-    line = '''
-    1 
-    2 
-    3
-    '''
-
     grammar = open('/home/nobel/Sync/python/workspace/onemodel/src/onemodel/dsl/grammars/onemodel.ebnf').read()
 
-    semantics = OnemodelSemantics()
+    # data = '(a+b)*10'
 
+    semantics = OnemodelSemantics()
     parser = tatsu.compile(grammar)
-    result = parser.parse(data, semantics=semantics)
+
+    try:
+        result = parser.parse(data, semantics=semantics)
+    except Exception as e:
+        print(str(e))
 
     print(result)
 
