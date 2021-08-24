c24cb9c52d0690e5c7c2f826eceeae0754e9a269
FernandoNobel <fersann1@upv.es>
2021-08-24 13:16:33 +0200

    Add restore path

--- src/onemodel/gui/path_field.py
+++ src/onemodel/gui/path_field.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QLineEdit, QErrorMessage
+from os import path
+from PyQt5.QtWidgets import QLineEdit, QMessageBox
 from PyQt5.QtCore import QDir
 
 class PathField:
@@ -9,13 +10,36 @@ class PathField:
         """
         self.window = window
         self.field = QLineEdit()
-        self.field.insert(QDir.homePath())
 
-        self.field.textChanged.connect(self.check_path)
+        self.field.setText(QDir.homePath())
+        self.last_valid_path = self.field.text()
+
+        self.field.returnPressed.connect(self.check_path)
+        self.field.editingFinished.connect(self.restore_path)
+
+    def set_path(self, path):
+        """ Set a new path.
+        """
+        self.last_valid_path = self.field.text()
+        self.field.setText(path)
 
     def check_path(self):
         """ Check that the path is valid.
         """
-        error_dialog = QErrorMessage(self.window)
-        error_dialog.showMessage('Oh no!')
-        error_dialog.show()
+        # Check if the new path existis.
+        if path.isdir(self.field.text()):
+            # Update the paths.
+            self.set_path(self.field.text())
+
+        else:
+            # If not, show error message.
+            title = 'Error Changing Folder'
+            msg = f'Cannot find folder "{self.field.text()}".\n'
+            msg += 'Check the spelling and try again.'
+
+            dialog = QMessageBox.about(self.window, title, msg) 
+
+    def restore_path(self):
+        """ Restore the path to the last valid path.
+        """
+        self.field.setText(self.last_valid_path)
