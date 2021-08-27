cf57c73c00290378b663c6abeb2d70bf29df9e70
FernandoNobel <fersann1@upv.es>
2021-08-27 11:16:29 +0200

    Add install_requieremetns

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="onemodel",
-    version="0.0.2",
+    version="0.0.3",
     author="Fernando Nobel Santos Navarro",
     author_email="fersann1@upv.es",
     description="OneModel package description.",
@@ -23,6 +23,13 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+          'tatsu',
+          'PyQt5',
+          'click',
+          'sympy',
+          'importlib-resources',
+    ],
     scripts=['bin/onemodel-cli.py'],
     include_package_data=True,
 )
