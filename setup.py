c3a82fcc0b70f241df8b52eaa6e0c7b317248f19
FernandoNobel <fersann1@upv.es>
2021-10-01 13:14:55 +0200

    v0.0.6 add examples action

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
         name="onemodel",
-        version="0.0.5",
+        version="0.0.6",
         author="Fernando Nobel Santos Navarro",
         author_email="fersann1@upv.es",
         description="OneModel package description.",
