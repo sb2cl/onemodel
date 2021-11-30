b041b5ff5403e2729c964cc743ff492a80d9faa9
FernandoNobel <fersann1@upv.es>
2021-11-30 11:34:33 +0100

    Updated to v0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
         name="onemodel",
-        version="0.0.9",
+        version="0.0.10",
         author="Fernando Nobel Santos Navarro",
         author_email="fersann1@upv.es",
         description="OneModel package description.",
