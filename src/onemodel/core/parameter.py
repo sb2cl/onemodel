541f82b4d8944c80e16f4236db7868b148933864
fernandonobel <fersann1@upv.es>
2022-06-27 18:31:53 +0200

    Add parameter to onemodel.core

---
+++ src/onemodel/core/parameter.py
@@ -0,0 +1,14 @@
+from onemodel.core.object import Object
+
+
+class Parameter (Object):
+
+    def __init__(self):
+        super().__init__()
+
+        self.isConstant = True
+        self.value = 0
+        self.units = None
+
+    def add_to_SBML(self):
+        pass
---
+++ tests/core/test_parameter.py
@@ -0,0 +1,13 @@
+from onemodel.core.parameter import Parameter
+
+
+def test_init():
+    result = Parameter()
+    assert isinstance(result, Parameter)
+
+def test_add_2_SBML():
+
+    p = Parameter()
+    model 
+
+    assert result == expected
