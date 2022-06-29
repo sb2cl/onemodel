17d4b636467c34479763d572c3040f01d5b7fdca
fernandonobel <fersann1@upv.es>
2022-06-29 16:43:46 +0200

    Add reactions and test_reactions

---
+++ src/onemodel/core/get_ast_names.py
@@ -0,0 +1,16 @@
+def get_ast_names(ast):
+    """ Returns the user defined names in a MathML ast.
+    """
+    names = []
+
+    if ast.isName():
+        names.append(ast.getName())
+
+    for i in range(ast.getNumChildren()):
+        child = ast.getChild(i)
+        child_names = get_ast_name(child)
+
+        for item in child_names:
+            names.append(item)
+
+    return names
