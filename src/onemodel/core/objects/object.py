32910a05f5aba36e72777633c5c753c84243efbb
fernandonobel <fersann1@upv.es>
2022-07-04 17:14:31 +0200

    Add test for reaction that requiere basename

--- src/onemodel/core/objects/object.py
+++ src/onemodel/core/objects/object.py
@@ -6,4 +6,4 @@ class Object(Namespace):
         super().__init__(None)
 
     def add_to_SBML_model(self, name, model):
-        raise Exception(f'Class "{type(self).__name__}" has not defined add_to_SBML method.')
+        pass
--- tests/core/test_onemodel.py
+++ tests/core/test_onemodel.py
@@ -1,6 +1,7 @@
 from xml.etree import ElementTree
 
 from onemodel.core.onemodel import OneModel
+from onemodel.core.objects.object import Object
 from onemodel.core.objects.species import Species
 from onemodel.core.objects.parameter import Parameter
 from onemodel.core.objects.reaction import Reaction
@@ -67,12 +68,14 @@ def test_ex01_simple_gene_expression():
     m.root['J3'].kinetic_law = 'k_p*mRNA'
     
     m.root['J4'] = Reaction()
-    m.root['J1'].reactants = ['protein']
+    m.root['J4'].reactants = ['protein']
     m.root['J4'].products = []
     m.root['J4'].kinetic_law = 'd_p*protein'
     
     result_string = m.get_SBML_string()
     result = ElementTree.fromstring(result_string)
+
+    print(result_string)
     
     expected_string = """<?xml version="1.0" encoding="UTF-8"?>
 <sbml xmlns="http://www.sbml.org/sbml/level3/version2/core" level="3" version="2">
@@ -99,9 +102,6 @@ def test_ex01_simple_gene_expression():
     </listOfParameters>
     <listOfReactions>
       <reaction id="J1" reversible="false">
-        <listOfReactants>
-          <speciesReference species="protein" constant="true"/>
-        </listOfReactants>
         <listOfProducts>
           <speciesReference species="mRNA" constant="true"/>
         </listOfProducts>
@@ -144,9 +144,9 @@ def test_ex01_simple_gene_expression():
         </kineticLaw>
       </reaction>
       <reaction id="J4" reversible="false">
-        <listOfModifiers>
-          <modifierSpeciesReference species="protein"/>
-        </listOfModifiers>
+        <listOfReactants>
+          <speciesReference species="protein" constant="true"/>
+        </listOfReactants>
         <kineticLaw>
           <math xmlns="http://www.w3.org/1998/Math/MathML">
             <apply>
@@ -168,9 +168,36 @@ def test_ex01_simple_gene_expression():
 def test_ex03_protein_constitutive():
     m = OneModel()
     
-    m.root['foo'] = Parameter()
-    m.root['foo']['bar'] = Parameter()
-  
+    m.root['A'] = Object()
+
+    m.root['A']['mRNA'] = Species()
+    m.root['A']['protein'] = Species()
+
+    m.root['A']['k_m'] = Parameter()
+    m.root['A']['d_m'] = Parameter()
+    m.root['A']['k_p'] = Parameter()
+    m.root['A']['d_p'] = Parameter()
+    
+    m.root['A']['J1'] = Reaction()
+    m.root['A']['J1'].reactants = []
+    m.root['A']['J1'].products = ['mRNA']
+    m.root['A']['J1'].kinetic_law = 'k_m'
+    
+    m.root['A']['J2'] = Reaction()
+    m.root['A']['J2'].reactants = ['mRNA']
+    m.root['A']['J2'].products = []
+    m.root['A']['J2'].kinetic_law = 'd_m*mRNA'
+    
+    m.root['A']['J3'] = Reaction()
+    m.root['A']['J3'].reactants = ['mRNA']
+    m.root['A']['J3'].products = ['mRNA', 'protein']
+    m.root['A']['J3'].kinetic_law = 'k_p*mRNA'
+    
+    m.root['A']['J4'] = Reaction()
+    m.root['A']['J4'].reactants = ['protein']
+    m.root['A']['J4'].products = []
+    m.root['A']['J4'].kinetic_law = 'd_p*protein'
+ 
     result_string = m.get_SBML_string()
     result = ElementTree.fromstring(result_string)
 
@@ -189,10 +216,74 @@ def test_ex03_protein_constitutive():
     <listOfCompartments>
       <compartment id="default_compartment" spatialDimensions="3" size="1" units="litre" constant="true"/>
     </listOfCompartments>
+    <listOfSpecies>
+      <species id="A__mRNA" compartment="default_compartment" initialConcentration="0" substanceUnits="mole" hasOnlySubstanceUnits="false" boundaryCondition="false" constant="false"/>
+      <species id="A__protein" compartment="default_compartment" initialConcentration="0" substanceUnits="mole" hasOnlySubstanceUnits="false" boundaryCondition="false" constant="false"/>
+    </listOfSpecies>
     <listOfParameters>
-      <parameter id="foo" value="0" units="per_second" constant="true"/>
-      <parameter id="foo__bar" value="0" units="per_second" constant="true"/>
+      <parameter id="A__k_m" value="0" units="per_second" constant="true"/>
+      <parameter id="A__d_m" value="0" units="per_second" constant="true"/>
+      <parameter id="A__k_p" value="0" units="per_second" constant="true"/>
+      <parameter id="A__d_p" value="0" units="per_second" constant="true"/>
     </listOfParameters>
+    <listOfReactions>
+      <reaction id="A__J1" reversible="false">
+        <listOfProducts>
+          <speciesReference species="A__mRNA" constant="true"/>
+        </listOfProducts>
+        <kineticLaw>
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <ci> A__k_m </ci>
+          </math>
+        </kineticLaw>
+      </reaction>
+      <reaction id="A__J2" reversible="false">
+        <listOfReactants>
+          <speciesReference species="A__mRNA" constant="true"/>
+        </listOfReactants>
+        <kineticLaw>
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <apply>
+              <times/>
+              <ci> A__d_m </ci>
+              <ci> A__mRNA </ci>
+            </apply>
+          </math>
+        </kineticLaw>
+      </reaction>
+      <reaction id="A__J3" reversible="false">
+        <listOfReactants>
+          <speciesReference species="A__mRNA" constant="true"/>
+        </listOfReactants>
+        <listOfProducts>
+          <speciesReference species="A__mRNA" constant="true"/>
+          <speciesReference species="A__protein" constant="true"/>
+        </listOfProducts>
+        <kineticLaw>
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <apply>
+              <times/>
+              <ci> A__k_p </ci>
+              <ci> A__mRNA </ci>
+            </apply>
+          </math>
+        </kineticLaw>
+      </reaction>
+      <reaction id="A__J4" reversible="false">
+        <listOfReactants>
+          <speciesReference species="A__protein" constant="true"/>
+        </listOfReactants>
+        <kineticLaw>
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <apply>
+              <times/>
+              <ci> A__d_p </ci>
+              <ci> A__protein </ci>
+            </apply>
+          </math>
+        </kineticLaw>
+      </reaction>
+    </listOfReactions>
   </model>
 </sbml>
     """
