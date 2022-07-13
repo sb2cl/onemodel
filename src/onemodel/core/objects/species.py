4ceaf823c8b38d6f5874dc0312cb3e4b581ece8e
fernandonobel <fersann1@upv.es>
2022-07-13 11:02:33 +0200

    Rework species

--- src/onemodel/core/objects/species.py
+++ src/onemodel/core/objects/species.py
@@ -3,17 +3,28 @@ from onemodel.core.objects.object import Object
 
 
 class Species(Object):
+    """A species is a value that changes during simulation time.
 
+    Parameters
+    ----------
+    compartment : :obj:`str`
+    initialConcentration : :obj:`float`
+    substanceUnits : :obj:`str`
+    constant : :obj:`bool`
+    boundaryCondition : :obj:`bool`
+    hasOnlySubstanceUnits : :obj:`bool`
+    """
     def __init__(self):
         super().__init__()
-        self.compartment = "default_compartment"
-        self.initialConcentration = 0
-        self.substanceUnits = "mole"
-        self.constant = False
-        self.boundaryCondition = False
-        self.hasOnlySubstanceUnits = False
+        self["compartment"] = "default_compartment"
+        self["initialConcentration"] = 0
+        self["substanceUnits"] = "mole"
+        self["constant"] = False
+        self["boundaryCondition"] = False
+        self["hasOnlySubstanceUnits"] = False
 
     def add_to_SBML_model(self, name, scope, model):
+        """Include this object into a SBML model. """
 
         fullname = scope.get_fullname(name)
 
@@ -30,32 +41,32 @@ class Species(Object):
         )
 
         check(
-            s.setCompartment(self.compartment), 
+            s.setCompartment(self["compartment"]), 
             f"set species {fullname} in default_compartment"
         )
 
         check(
-            s.setConstant(self.constant), 
+            s.setConstant(self["constant"]), 
             f'set "constant" attribute on {fullname}'
         )
 
         check(
-            s.setInitialConcentration(self.initialConcentration), 
+            s.setInitialConcentration(self["initialConcentration"]), 
             f"set initial amount for {fullname}"
         )
 
         check(
-            s.setSubstanceUnits(self.substanceUnits), 
+            s.setSubstanceUnits(self["substanceUnits"]), 
             f"set substance units for {fullname}"
         )
 
         check(
-            s.setBoundaryCondition(self.boundaryCondition), 
+            s.setBoundaryCondition(self["boundaryCondition"]), 
             f'set "boundaryCondition" on {fullname}'
         )
 
         check(
-            s.setHasOnlySubstanceUnits(self.hasOnlySubstanceUnits),
+            s.setHasOnlySubstanceUnits(self["hasOnlySubstanceUnits"]),
             f'set "hasOnlySubstanceUnits" on {fullname}',
         )
 
