a27b955ee386e27165fa531ba4803c389e31a0d2
fernandonobel <fersann1@upv.es>
2022-09-21 15:49:42 +0200

    add test for find_module

--- tests/objects/test_module.py
+++ tests/objects/test_module.py
@@ -3,3 +3,10 @@ from onemodel.objects.module import Module
 def test_init():
     result = Module()
     assert isinstance(result, Module)
+
+def test_find_module():
+    module_name = "ex01_simple_gene_expression"
+    result = find_module(module_name)
+    expected = "/home/nobel/Sync/projects/python/onemodel/examples/ex01_simple_gene_expression.one" 
+
+    assert result == expected
