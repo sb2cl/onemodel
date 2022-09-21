15470346f7949410a6f725f4567e16bd039bd9b5
fernandonobel <fersann1@upv.es>
2022-09-21 16:01:35 +0200

    Changed test_module to work in a temporal directory

--- tests/objects/test_module.py
+++ tests/objects/test_module.py
@@ -1,13 +1,30 @@
+import os
+import pytest
+from distutils.dir_util import copy_tree
+from pathlib import Path
+
 from onemodel.objects.module import Module
 from onemodel.objects.module import find_module
+
+
+@pytest.fixture
+def tmp_examples_dir(tmpdir):
+    result = Path(tmpdir)
+    copy_example_files_into(result)
+    return result
+
+def copy_example_files_into(path):
+    copy_tree("./", str(path))
 
 def test_init():
     result = Module()
     assert isinstance(result, Module)
 
-def test_find_module():
+def test_find_module(tmp_examples_dir):
+    os.chdir(tmp_examples_dir)
+
     module_name = "ex01_simple_gene_expression"
     result = find_module(module_name)
-    expected = "/home/nobel/Sync/projects/python/onemodel/examples/ex01_simple_gene_expression.one" 
+    expected = tmp_examples_dir / "ex01_simple_gene_expression.one" 
 
-    assert result == expected
+    assert result == str(expected)
