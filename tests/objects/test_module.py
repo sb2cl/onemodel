b4da32b36d75e97a23ecac5d8c5be280de6f1f5b
fernandonobel <fersann1@upv.es>
2022-09-26 13:03:28 +0200

    Add test_module folder with files for testing importing

--- tests/objects/test_module.py
+++ tests/objects/test_module.py
@@ -16,26 +16,27 @@ def tmp_examples_dir(tmpdir):
     return result
 
 def copy_example_files_into(path):
-    copy_tree("./", str(path))
+    path_test_module = os.path.dirname(__file__) + "/test_module"
+    copy_tree(path_test_module, str(path))
 
 def test_init():
     result = Module()
     assert isinstance(result, Module)
 
 def test_find_module(tmp_examples_dir):
-    os.chdir(tmp_examples_dir)
+    os.chdir(tmp_examples_dir / "src")
 
-    module_name = "ex01_simple_gene_expression"
+    module_name = "module_1"
     result = find_module(module_name)
-    expected = tmp_examples_dir / "ex01_simple_gene_expression.one" 
+    expected = tmp_examples_dir / "src/module_1.one" 
 
     assert result == str(expected)
 
 def test_load_module_1(tmp_examples_dir):
-    os.chdir(tmp_examples_dir)
+    os.chdir(tmp_examples_dir / "src")
     walker = OneModelWalker()
 
-    module_name = "ex01_simple_gene_expression"
+    module_name = "module_1"
     import_name = None
     assign_name = None
 
@@ -49,14 +50,14 @@ def test_load_module_1(tmp_examples_dir):
     assert isinstance(result, Module)
     assert result["__name__"] == module_name
     assert result["__file__"] == find_module(module_name)
-    assert result["k_m"]["value"] == 1
+    assert result["add"]["argument_names"] == ['a', 'b']
     assert walker.onemodel[module_name] == result  
 
 def test_load_module_2(tmp_examples_dir):
-    os.chdir(tmp_examples_dir)
+    os.chdir(tmp_examples_dir / "src")
     walker = OneModelWalker()
 
-    module_name = "ex01_simple_gene_expression"
+    module_name = "module_1"
     import_name = None
     assign_name = "foo"
 
@@ -70,11 +71,11 @@ def test_load_module_2(tmp_examples_dir):
     assert walker.onemodel[assign_name] == result
 
 def test_load_module_3(tmp_examples_dir):
-    os.chdir(tmp_examples_dir)
+    os.chdir(tmp_examples_dir / "src")
     walker = OneModelWalker()
 
-    module_name = "ex01_simple_gene_expression"
-    import_name = "mRNA"
+    module_name = "module_1"
+    import_name = "add"
     assign_name = "foo"
 
     result = load_module(
@@ -87,11 +88,11 @@ def test_load_module_3(tmp_examples_dir):
     assert walker.onemodel[assign_name] == result[import_name]
 
 def test_load_module_4(tmp_examples_dir):
-    os.chdir(tmp_examples_dir)
+    os.chdir(tmp_examples_dir / "src")
     walker = OneModelWalker()
 
-    module_name = "ex01_simple_gene_expression"
-    import_name = "mRNA"
+    module_name = "module_1"
+    import_name = "add"
     assign_name = None
 
     result = load_module(
---
+++ tests/objects/test_module/src/main.py
@@ -0,0 +1,3 @@
+import module_1
+
+module_1.add(2, 3)
