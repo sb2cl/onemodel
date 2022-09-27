51491494f33a3e10770773beb2bef2873aa1dfbc
fernandonobel <fersann1@upv.es>
2022-09-27 18:16:01 +0200

    Add run and install commands to onemodel

--- src/onemodel/__main__.py
+++ src/onemodel/__main__.py
@@ -2,13 +2,24 @@ import sys
 
 from onemodel.repl import Repl
 from onemodel.onemodel_walker import load_file
+from onemodel.package_manager import PackageManager
 
 def main():
     if len( sys.argv ) > 1:
-        filename = sys.argv[1]
-        onemodel = load_file(filename)
-        sbml = onemodel.get_SBML_string()
-        print(sbml)
+        cmd = sys.argv[1]
+
+        if cmd == "run":
+            filename = sys.argv[2]
+            onemodel = load_file(filename)
+            sbml = onemodel.get_SBML_string()
+            print(sbml)
+
+        if cmd == "install":
+            pm = PackageManager()
+            pm.load_toml_file()
+            pm.install_dependencies()
+
+            print("Installed dependencies")
 
     else:
         repl = Repl()
